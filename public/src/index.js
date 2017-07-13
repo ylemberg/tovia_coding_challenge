0b37c48d8d8b5997cd4148661288db531a027f23
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-13 10:02:46 -0700

    [setup] OMG finally got react-toolbox to work

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Button } from 'react-toolbox/lib/button'
+
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,80 @@
-const webpack = require('webpack')
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 
 const SRC_DIR = path.resolve(__dirname, 'public/src')
 const BUILD_DIR = path.resolve(__dirname, 'public/build')
 
-module.exports = {
-  entry: path.resolve(SRC_DIR, 'index.js'),
+const settings = {
+  entry: {
+    bundle: [
+      "react-hot-loader/patch",
+      "./public/src/index.js"
+    ]
+  },
   output: {
-    filename: 'bundle.js',
+    filename: "[name].js",
+    publicPath: "/",
     path: BUILD_DIR
   },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      test: /\.js$/,
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['transform-class-properties']
-      }
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
+  resolve: {
+    extensions: [".js", ".json", ".css"]
   },
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ],
+          plugins: [
+            "transform-node-env-inline"
+          ],
+          env: {
+            development: {
+              plugins: ["react-hot-loader/babel"]
+            }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve("src/www"),
+    publicPath: "http://localhost:8080/", // full URL is necessary for Hot Module Replacement if additional path will be added.
+    quiet: false,
+    hot: true,
+    historyApiFallback: true,
+    inline: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  ],
   watch: true
 }
+
+module.exports = settings
