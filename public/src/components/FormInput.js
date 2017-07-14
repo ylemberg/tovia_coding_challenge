ccf25c46930bc5a78b3fba1511ea605375d57eeb
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-13 17:52:17 -0700

    [prog] Add a few more input fields

--- public/src/components/FormInput.js
+++ public/src/components/FormInput.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+
 import Input from 'react-toolbox/lib/input'
 import Avatar from 'react-toolbox/lib/avatar';
+import DatePicker from 'react-toolbox/lib/date_picker';
 
 import { avatar, nameLabel } from '../styling'
 
@@ -8,7 +10,9 @@ export default class FormInput extends Component {
   constructor() {
     super()
     this.state = {
-      name: ''
+      name: '',
+      message: '',
+      date: ''
     }
   }
 
@@ -21,8 +25,10 @@ export default class FormInput extends Component {
       <div>
         <div style={avatar}>S</div>
         <div style={nameLabel}>
-          <Input  type='text' label='Name' name='name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')} />
+          <Input type='text' label='Name' name='name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')} />
         </div>
+        <Input required type='text' label='Message' name='message' value={this.state.message} onChange={this.handleChange.bind(this, 'message')} maxLength={120}/>
+        <DatePicker label='Expiration Date' sundayFirstDayOfWeek onChange={this.handleChange.bind(this, 'date')} value={this.state.date} />
       </div>
     )
   }
